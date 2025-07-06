bc65932d7ed96711d06bff2615977467859267bb
abdo1rahman <abdoasem2055@gmail.com>
2025-07-06 19:04:08 +0300

    ...

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
